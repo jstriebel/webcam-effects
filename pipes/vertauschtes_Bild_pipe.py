23b48eb252197f1ae66f58291110f6f71489079c
luchtele <lea.luchterhand@gmail.com>
2017-08-07 18:18:49 +0200

    Style change

--- pipes/vertauschtes_Bild_pipe.py
+++ pipes/vertauschtes_Bild_pipe.py
@@ -3,9 +3,9 @@ import numpy as np
 
 class VertauschtesBildPipe(Pipe):
     def pipe(self, frame):
-        newframe= np.zeros((480,640,3),dtype= np.uint8)
-        newframe[0:240,:,:]= frame[240:480,:,:]
-        newframe[240:480,:,:] =frame[0:240,:,:]
+        newframe = np.zeros((480, 640, 3), dtype = np.uint8)
+        newframe[0: 240, : , : ] = frame[240: 480, : , : ]
+        newframe[240: 480, : , : ] = frame[0: 240, : , : ]
         return newframe
 
     def __enter__(self):
