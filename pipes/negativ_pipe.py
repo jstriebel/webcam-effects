921a279f25a706741cd5fe1d9fedef68ab1e3548
luchtele <lea.luchterhand@gmail.com>
2017-08-07 18:05:26 +0200

    swaps colours

---
+++ pipes/negativ_pipe.py
@@ -0,0 +1,14 @@
+from pipes.pipe import Pipe
+import numpy as np
+
+class NegativPipe(Pipe):
+    def pipe(self, img):
+        whiteframe= np.ones(img.shape,dtype= np.uint8)*255
+        newframe= whiteframe - img                               
+        return newframe
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exit_type, value, traceback):
+        pass
