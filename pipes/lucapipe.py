1ef76be5ce7de13422db9ff2c2c05d772338a78c
lucaelbracht <luca.elbracht@web.de>
2017-08-07 18:46:22 +0200

    Filter mit threshold und erode/dilate

--- pipes/lucapipe.py
+++ pipes/lucapipe.py
@@ -5,14 +5,10 @@ import cv2
 class LucaPipe(Pipe):
     def pipe(self, data):
         gray = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
-        
         _, gray = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
-        #gray = (gray[:,:,] > 128) * 255
         kernel = np.ones((3,3),np.uint8)
         gray = cv2.erode(gray, kernel, iterations = 1)
         gray = cv2.dilate(gray, kernel, iterations = 1)
-
-
         return gray
 
     def __enter__(self):
