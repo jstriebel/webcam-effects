44d51c49402f84f88da99d9d5965ead1cdb04d56
Johannes <johannes@Johanness-MacBook-Pro.local>
2017-08-07 18:40:12 +0200

    coloured_picture

--- pipes/coloured_picture.py
+++ pipes/coloured_picture.py
@@ -5,10 +5,10 @@ class BoringPipe(Pipe):
     def pipe(self, data):
         
         shape = data.shape
-        b = shape[1]//3
-        data[:, b:2*b, 2] = 200
+        b = shape[1] // 3
+        data[:, b:2 * b, 2] = 200
         data[:, 0:b, 0] = 200
-        data[:, 2*b:3*b, 1] = 200
+        data[:, 2 * b:3 * b, 1] = 200
          
         return data
 
