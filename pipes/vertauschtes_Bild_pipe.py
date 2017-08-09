0941b24b879b4276d395cdecc8c75b4f076e90a3
Norman Rzepka <code@normanrz.com>
2017-08-09 22:10:37 +0200

    use integer division for yhalfsize

--- pipes/vertauschtes_Bild_pipe.py
+++ pipes/vertauschtes_Bild_pipe.py
@@ -4,7 +4,7 @@ import numpy as np
 class VertauschtesBildPipe(Pipe):
     def pipe(self, img):
         ysize = img.shape[0]
-        yhalfsize = ysize / 2
+        yhalfsize = ysize // 2
         newframe = np.zeros(img.shape, dtype=np.uint8)
         newframe[:yhalfsize, :, :] = img[yhalfsize:ysize, :, :]
         newframe[yhalfsize:ysize, :, :] = img[:yhalfsize, :, :]
